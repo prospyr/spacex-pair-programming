9d54faa951cdb8e9920cbfb5f7a7dfd4a7ec751d
Peter McKinley <peter.michael.mckinley@gmail.com>
2021-12-13 12:10:29 -0500

    Added gql file

---
+++ src/api/queries/upcomingLaunches.js
@@ -0,0 +1,10 @@
+const UPCOMING_LAUNCHES = gql`
+  query Launches($limit: Int) {
+    launches(limit: $limit) {
+      mission_name
+      launch_date_local
+    }
+  }
+`;
+
+export default UPCOMING_LAUNCHES;
